e663885a4e46c4f81a38351e863a6e792846db9f
madmaxoft <github@xoft.cz>
2013-12-06 22:31:57 +0100

    Galleries and config are loaded and verified. Player interaction prevention is working.

--- Gallery.lua
+++ Gallery.lua
@@ -7,13 +7,106 @@
 
 
 
--- The main list of galleries available
+--- The main list of galleries available
 local g_Galleries = {};
 
--- The configuration
+--- The configuration
 local g_Config = {};
 
+--- The per-world per-player list of owned areas. Access as "g_Areas[WorldName][PlayerEntityID]"
+local g_PlayerAreas = {};
+
+--- The file from which the galleries and the configuration is read
 local CONFIG_FILE = "Galleries.cfg"
+
+
+
+
+-- Static data:
+--- Parameters that are required for each gallery:
+local g_GalleryRequiredParams =
+{
+	{ Name = "MinX", Type = "number", },
+	{ Name = "MinZ", Type = "number", },
+	{ Name = "MaxX", Type = "number", },
+	{ Name = "MaxZ", Type = "number", },
+	{ Name = "FillStrategy", Type = "string", },
+	{ Name = "WorldName", Type = "string", }
+} ;
+
+--- Parameters that are optional for each gallery (so that the type is checked):
+local g_GalleryOptionalParams =
+{
+	{ Name = "AreaTemplate", Type = "string" },
+	{ Name = "AreaEdge", Type = "number" },
+} ;
+
+
+
+
+
+--- Returns true if the gallery has all the minimum settings it needs
+-- a_Index is used instead of gallery name if the name is not present
+-- Also loads the gallery's schematic file
+local function CheckGallery(a_Gallery, a_Index)
+	-- Check if the name is given:
+	if (a_Gallery.Name == nil) then
+		LOGWARNNIG("Gallery #" .. a_Index .. " doesn't have a Name, disabling it.");
+		return false;
+	end
+	
+	-- Check all required parameters by name and type:
+	for idx, param in ipairs(g_GalleryRequiredParams) do
+		local p = a_Gallery[param.Name];
+		if (p == nil) then
+			LOGWARNING("Gallery \"" .. a_Gallery.Name .. "\" is missing a required parameter '" .. param.Name .."'. Disabling the gallery.");
+			return false;
+		end
+		if (type(p) ~= param.Type) then
+			LOGWARNING("Gallery \"" .. a_Gallery.Name .. "\"'s parameter \"" .. param.Name .."\" is wrong type. Expected " .. param.Type .. ", got " .. type(p) ..". Disabling the gallery.");
+			return false;
+		end
+	end
+	
+	-- Check all optional parameters' types:
+	for idx, param in ipairs(g_GalleryOptionalParams) do
+		local p = a_Gallery[param.Name];
+		if (p ~= nil) then
+			if (type(p) ~= param.Type) then
+				LOGWARNING("Gallery \"" .. a_Gallery.Name .. "\"'s parameter \"" .. param.Name .."\" is wrong type. Expected " .. param.Type .. ", got " .. type(p) ..". Disabling the gallery.");
+				return false;
+			end
+		end
+	end
+	
+	-- Assign the world:
+	a_Gallery.World = cRoot:Get():GetWorld(a_Gallery.WorldName);
+	if (a_Gallery.World == nil) then
+		LOGWARNING("Gallery \"" .. a_Gallery.Name .. "\" specifies an unknown world '" .. a_Gallery.WorldName .. "\".");
+		return false;
+	end
+
+	-- Load the schematic, if requested:
+	local AreaTemplate = a_Gallery["AreaTemplate"];
+	if (AreaTemplate ~= nil) then
+		local Schematic = cBlockArea();
+		if (Schematic == nil) then
+			LOGWARNING("Cannot create the template schematic representation");
+			return true;
+		end
+		if not(Schematic:LoadFromSchematicFile(AreaTemplate)) then
+			LOGWARNING("Gallery \"" .. a_Gallery.Name .. "\"'s AreaTemplate failed to load from \"" .. AreaTemplate .. "\".");
+			return false;
+		end
+		a_Gallery.AreaTemplateSchematic = Schematic;
+	end
+	
+	-- Apply defaults:
+	a_Gallery.AreaEdge = a_Gallery.AreaEdge or 2;
+	
+	-- All okay
+	return true;
+end
 
 
 
@@ -21,7 +114,14 @@ local CONFIG_FILE = "Galleries.cfg"
 
 --- Verifies that each gallery has all the minimum settings it needs
 local function VerifyGalleries()
-	-- TODO
+	-- Filter out galleries that are not okay:
+	local GalleriesOK = {};
+	for idx, gallery in ipairs(g_Galleries) do
+		if (CheckGallery(gallery, idx)) then
+			table.insert(GalleriesOK, gallery);
+		end
+	end
+	g_Galleries = GalleriesOK;
 end
 
 
@@ -41,24 +141,47 @@ end
 local function LoadConfig()
 	if not(cFile:Exists(CONFIG_FILE)) then
 		-- No file to read from, silently bail out
+		-- But first copy our example file to the folder, to let the admin know the format:
+		local PluginFolder = cPluginManager:Get():GetCurrentPlugin():GetLocalFolder()
+		cFile:Copy(PluginFolder .. "/example.cfg", (CONFIG_FILE:gsub(".cfg", ".example.cfg")));
 		return;
 	end
 	
 	-- Load and compile the config file:
-	local f, err = loadfile(CONFIG_FILE);
-	if (f == nil) then
+	local cfg, err = loadfile(CONFIG_FILE);
+	if (cfg == nil) then
 		LOGWARNING("Cannot open " .. CONFIG_FILE .. ": " .. err);
 		LOGWARNING("No galleries were loaded");
 		return;
 	end
 	
-	-- Sandbox the loaded file, just in case, and execute:
+	-- Execute the loaded file in a sandbox:
+	-- This is Lua-5.1-specific and won't work in Lua 5.2!
 	local SecureEnvironment = {};
-	setfenv(f, SecureEnvironment);
-	f();
+	setfenv(cfg, SecureEnvironment);
+	cfg();
 	
-	-- Retrieve the values we want:
+	-- Retrieve the values we want from the sandbox:
 	g_Galleries, g_Config = SecureEnvironment.Galleries, SecureEnvironment.Config;
+	if (g_Galleries == nil) then
+		LOGWARNING("Galleries not found in the config file '" .. CONFIG_FILE .. "'. Gallery plugin inactive.");
+		g_Galleries = {};
+	end
+	if (g_Config == nil) then
+		LOGWARNING("Config not found in the config file '" .. CONFIG_FILE .. "'. Using defaults.");
+		g_Config = {};  -- Defaults will be inserted by VerifyConfig()
+	end
+end
+
+
+
+
+
+--- Claims an area in a gallery for the specified player. Returns a table describing the area
+-- If there's no space left in the gallery, returns nil and error message
+local function ClaimArea(a_Player, a_Gallery)
+	-- TODO
+	return nil, "Not implemented yet";
 end
 
 
@@ -92,6 +215,15 @@ end
 
 
 
+local function HandleCmdGoto(a_Split, a_Player)
+	-- TODO
+	return true;
+end
+
+
+
+
+
 --- The list of subcommands, their handler functions and metadata:
 local g_Subcommands =
 {
@@ -115,6 +247,13 @@ local g_Subcommands =
 		Help = "lists all your areas [in the <gallery>]",
 		Permission = "gallery.my",
 		Handler = HandleCmdMy,
+	},
+	{
+		Cmd = "goto",
+		Params = "<areaID>",
+		Help = "teleports you to specified gallery area",
+		Permission = "gallery.goto",
+		Handler = HandleCmdGoto,
 	},
 } ;
 
@@ -166,7 +305,155 @@ end
 
 
 
--- All the initialization code should be here:
+--- Loads the areas for a single player in the specified world
+local function LoadPlayerAreas(a_WorldName, a_PlayerName)
+	-- TODO
+	
+	--DEBUG: return a dummy area to test prevention:
+	return
+	{
+		{
+			MinX = 102,
+			MaxX = 115,
+			MinZ = 102,
+			MaxZ = 115,
+		}
+	};
+end
+
+
+
+
+local function LoadAllPlayersAreas()
+	cRoot:Get():ForEachWorld(
+		function (a_World)
+			local WorldAreas = {}
+			a_World:ForEachPlayer(
+				function (a_Player)
+					WorldAreas[a_Player:GetUniqueID()] = LoadPlayerAreas(a_World, a_Player:GetName());
+				end
+			);
+			g_PlayerAreas[a_World:GetName()] = WorldAreas;
+		end
+	);
+end
+
+
+
+
+
+local function OnDisconnect(a_Player, a_Reason)
+	-- TODO: Remove the player's areas from the global list
+end
+
+
+
+
+
+local function FindGalleryByCoords(a_World, a_BlockX, a_BlockZ)
+	for idx, gallery in ipairs(g_Galleries) do
+		if (gallery.World == a_World) then
+			if (
+				(a_BlockX >= gallery.MinX) and
+				(a_BlockX <= gallery.MaxX) and
+				(a_BlockZ >= gallery.MinZ) and
+				(a_BlockZ <= gallery.MaxZ)
+			) then
+				return gallery;
+			end
+		end
+	end
+	return nil;
+end
+
+
+
+
+
+local function IsInArea(a_Area, a_BlockX, a_BlockZ)
+	return (
+		(a_BlockX >= a_Area.MinX) and
+		(a_BlockX <= a_Area.MaxX) and
+		(a_BlockZ >= a_Area.MinZ) and
+		(a_BlockZ <= a_Area.MaxZ)
+	)
+end
+
+
+
+
+
+local function CanPlayerInteractWithBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ)
+	-- If the player is outside all galleries, bail out:
+	local Gallery = FindGalleryByCoords(a_Player:GetWorld(), a_BlockX, a_BlockZ);
+	if (Gallery == nil) then
+		-- Allowed to do anything outside the galleries
+		return true;
+	end
+	
+	-- Inside a gallery, retrieve the areas:
+	local Areas = g_PlayerAreas[a_Player:GetWorld():GetName()][a_Player:GetUniqueID()];
+	if (Areas == nil) then
+		-- The player is an admin who can interact with anything anywhere
+		return true;
+	end
+	
+	for idx, area in ipairs(Areas) do
+		if (IsInArea(area, a_BlockX, a_BlockZ)) then
+			-- This player's area, allow them
+			return true;
+		end
+	end
+	
+	-- Not this player's area, disallow:
+	return false;
+end
+
+
+
+
+
+local function OnPlayerLeftClick(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_Status)
+	if (CanPlayerInteractWithBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ)) then
+		return false;
+	end
+	a_Player:SendMessage("You are not allowed to dig here");
+	return true;
+end
+
+
+
+
+local function OnPlayerRightClick(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_CursorX, a_CursorY, a_CursorZ, a_Status)
+	if (a_BlockFace < 0) then
+		-- This really means "use item" and no valid coords are given
+		-- TODO: We want to disallow using buckets and doors etc, too (see FS #405)
+		return false;
+	end
+	
+	local BlockX, BlockY, BlockZ = AddFaceDirection(a_BlockX, a_BlockY, a_BlockZ, a_BlockFace);
+	if (CanPlayerInteractWithBlock(a_Player, BlockX, BlockY, BlockZ)) then
+		return false;
+	end
+	a_Player:SendMessage("You are not allowed to build here");
+	return true;
+end
+
+
+
+
+
+function OnPlayerSpawned(a_Player)
+	-- Read this player's areas for this world:
+	g_PlayerAreas[a_Player:GetWorld():GetName()][a_Player:GetUniqueID()] = LoadPlayerAreas(a_Player);
+	return false;
+end
+
+
+
+
+
+-- All the initialization code should be down here, global:
 
 -- Load the config
 LoadConfig();
@@ -175,11 +462,20 @@ LoadConfig();
 VerifyGalleries();
 VerifyConfig();
 
+-- Load per-player list of areas for all currently connected players:
+LoadAllPlayersAreas();
+
 -- Register the generic "gallery" command that will actually handle all the /gallery commands:
 cPluginManager.BindCommand(g_Config.CommandPrefix, "gallery.*", HandleGalleryCmd, "");
 
 -- Register the subcommands, so that they are listed in the help:
 RegisterSubcommands();
+
+-- Hook to the player interaction events so that we can disable them:
+cPluginManager.AddHook(cPluginManager.HOOK_DISCONNECT,         OnDisconnect);
+cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_LEFT_CLICK,  OnPlayerLeftClick);
+cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_RIGHT_CLICK, OnPlayerRightClick);
+cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_SPAWNED,     OnPlayerSpawned);
 
 
 

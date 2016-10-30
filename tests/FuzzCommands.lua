5efbb17d6abcc7d35616c29bcd1b8e814b1f2248
Mattes D <github@xoft.cz>
2016-10-30 14:29:33 +0100

    Added CuberitePluginChecker on Travis CI.

---
+++ tests/FuzzCommands.lua
@@ -0,0 +1,51 @@
+-- FuzzCommands.lua
+
+-- Defines a scenario for fuzzing the commands
+
+
+
+
+
+scenario
+{
+	name = "Fuzz all commands",
+	redirect -- Redirect files / folders
+	{
+		["Galleries.cfg"] = "tests/FuzzCommands.cfg",
+		["Galleries.example.cfg"] = "tests/FuzzCommands.example.cfg",
+		["Galleries.sqlite"] = "tests/FuzzCommands.sqlite",
+	},
+	world  -- Create a world
+	{
+		name = "world",
+	},
+	initializePlugin(),
+	connectPlayer  -- Simulate a player connection
+	{
+		name = "player1",
+		worldName = "world",
+	},
+	connectPlayer
+	{
+		name = "player2",
+		worldName = "world",
+	},
+	playerCommand  -- Execute a command handler on behalf of this player
+	{
+		playerName = "player1",
+		command = "/gallery claim first",
+	},
+	fuzzAllCommands  -- fuzz all registered commands
+	{
+		playerName = "player1",
+		choices =
+		{
+			"world",
+			"first",
+			"player1",
+			"player2",
+			"1",
+		},
+		maxLen = 2,  -- Max number of elements chosen from "choices"
+	},
+}
